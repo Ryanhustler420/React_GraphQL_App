4afa3c258a01143c44a027c85d69f78db0bfe251
Gaurav Gupta <gouravgupta840@gmail.com>
2019-01-10 16:38:24 +0530

    Items Creation and Prisma Yoga Flow

--- app/backend/src/resolvers/Mutation.js
+++ app/backend/src/resolvers/Mutation.js
@@ -1,29 +1,33 @@
 const Mutations = {
-  createDog (parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    
-    // create a dog!
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
+  async createItem (parent, args, ctx, info) {
+    // TODO: Check if they are logged in
 
-    return newDog
+    const item = await ctx.db.mutation.createItem (
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+
+    return item;
   },
 };
-
+// info => query
 module.exports = Mutations;
 
-// # Run this in play ground
-    
-// query getDogsArray {
-    // dogs {
-    //      name
-    // }
-// }
+// Run this query on playground
 
-// mutation CreateADog {
-    // createDog(name: "Buttu") {
-    //      name
-    // }
+// mutation {
+//   createItem(
+//     title: "Second"
+//     description: "Testing Second Item"
+//     image: "dog1.jpg"
+//     largeImage: "doggg.jpg"
+//     price: 1000
+//   ) {
+//     id 
+//     title
+//   }
 // }
-
-// {name : "Buttu"}
--- app/backend/src/resolvers/Query.js
+++ app/backend/src/resolvers/Query.js
@@ -1,19 +1,23 @@
-// Query.js only use for Read [pulling] data from Data base not for Create Update Delete
+// here we can directly send the query to prisma backend if the query is same that prisma expect and doen'nt containts any filter and authentcation at all then forward the request directly to prisma API
+
+
+const { forwardTo } = require('prisma-binding');
+
 const Query = {
-  // dogs: function() { // code... }
-  dogs (parent, ages, ctx, info) {
-    //  return [{name: 'Snickers'}, {name: 'Sunny'}];
-    global.dogs = global.dogs || [];
-    return global.dogs;
-  },
+  // forwarding to prisma API directly
+  items: forwardTo('db')
+
+  // async items (parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items ();
+  //   return items;
+  // },
 };
 
 module.exports = Query;
 
-// ## run this in GraphQl playground
-// query {
-//      dogs {
-//          name
-//      }
+// query allItems {
+//   items {
+//     id
+//     title
+//   }
 // }
-//
