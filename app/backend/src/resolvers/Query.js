2b05416f464d89ae39e8aa98a4d23209ab0c8886
Gaurav Gupta <gouravgupta840@gmail.com>
2019-01-10 15:42:09 +0530

    first Query and Mutation WIth Yoga with local storage

--- app/backend/src/resolvers/Query.js
+++ app/backend/src/resolvers/Query.js
@@ -1,3 +1,19 @@
-const Query = {};
+// Query.js only use for Read [pulling] data from Data base not for Create Update Delete
+const Query = {
+  // dogs: function() { // code... }
+  dogs (parent, ages, ctx, info) {
+    //  return [{name: 'Snickers'}, {name: 'Sunny'}];
+    global.dogs = global.dogs || [];
+    return global.dogs;
+  },
+};
 
 module.exports = Query;
+
+// ## run this in GraphQl playground
+// query {
+//      dogs {
+//          name
+//      }
+// }
+//
