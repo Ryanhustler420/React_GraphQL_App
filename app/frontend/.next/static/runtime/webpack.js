7c630cb8ba17cef3c9f2edb1923b284469b8edde
Gaurav Gupta <gouravgupta840@gmail.com>
2019-01-13 04:16:43 +0530

    Created Template for creating Item

--- app/frontend/.next/static/runtime/webpack.js
+++ app/frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1b09d8026e9235afdade";
+/******/ 	var hotCurrentHash = "b041eabdc1afd57f7f8b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
