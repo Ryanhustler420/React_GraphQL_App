e3a4723c51d370611e1f0e8823e9f53d57135aed
Gaurav Gupta <gouravgupta840@gmail.com>
2019-01-14 22:35:28 +0530

    Register user mutation and cookie pasrse middleware implementaio

--- app/backend/src/resolvers/Mutation.js
+++ app/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require ('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem (parent, args, ctx, info) {
     // TODO: Check if they are logged in
@@ -39,6 +42,52 @@ const Mutations = {
     // delete it
     return ctx.db.mutation.deleteItem ({where}, info);
   },
+  async signup (parent, args, ctx, info) {
+    // lowercase their email
+    args.email = args.email.toLowerCase ();
+    // hash their password
+    const password = await bcrypt.hash (args.password, 10);
+    // create a user in the database
+    const user = await ctx.db.mutation.createUser (
+      {
+        data: {
+          ...args,
+          password,
+          permission: {set: ['USER']},
+        },
+      },
+      info
+    );
+    // create the JWT token for theme
+    const token = jwt.sign({userId: user.id}, process.env.APP_SECRET);
+    // We set the jwt as a cookie on the response
+    ctx.response.cookie('token',token,{
+      // (httpOnly:) you cannot access this token via 
+      // javascript, because you could get third party 
+      // javascript on your website or you could get a ROG code extension. 
+      // you do not want you JS file to access this cookies.
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+    //  Finalllllly we return the user to the browser
+    return user;
+  },
 };
 // info => query
 module.exports = Mutations;
+
+// run this is playground
+
+// # Write your query or mutation here
+// mutation createUser {
+//   signup(
+//     email: "gouravgupta840@gmail.com",
+//     name: "Gaurav Gupta",
+//     password: "123456"
+//   ) {
+//     name
+//     email
+//     password
+//     permission
+//   }
+// }
