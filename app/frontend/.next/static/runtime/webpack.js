7d556e0573e628f4a4bc711df3a90b7a7e16af2b
Gaurav Gupta <gouravgupta840@gmail.com>
2019-01-15 16:44:08 +0530

    Update Nav when login

--- app/frontend/.next/static/runtime/webpack.js
+++ app/frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b9075baafc66440a6f2c";
+/******/ 	var hotCurrentHash = "8795c614f823be762fb9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
