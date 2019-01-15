4f19288a564f99adcc01bf189ef5ed1c52f63563
Gaurav Gupta <gouravgupta840@gmail.com>
2019-01-15 16:06:58 +0530

    Update the UI async Using refetchQuery

--- app/frontend/.next/static/runtime/webpack.js
+++ app/frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d935040059d57ae19a36";
+/******/ 	var hotCurrentHash = "b9075baafc66440a6f2c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
