823ec0673f2dcbe1dfc9279e6bf68d0549fdfbcb
Gaurav Gupta <gouravgupta840@gmail.com>
2019-01-18 20:56:24 +0530

    Signout Backend

--- app/frontend/.next/static/runtime/webpack.js
+++ app/frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8795c614f823be762fb9";
+/******/ 	var hotCurrentHash = "8c8337b6922e8f6bf3ee";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
