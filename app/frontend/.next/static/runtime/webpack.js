4493c50b88597ff4b76f682b0e01699ccb247fb9
Gaurav Gupta <gouravgupta840@gmail.com>
2019-01-22 22:05:58 +0530

    Made Async Called to Apollo using ApolloConsumer and Fetch Data

--- app/frontend/.next/static/runtime/webpack.js
+++ app/frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "52e9b251f460baa0c364";
+/******/ 	var hotCurrentHash = "f55ffa17079da64ca8f7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- app/frontend/components/Search.js
+++ app/frontend/components/Search.js
@@ -6,12 +6,44 @@ import gql from 'graphql-tag';
 import debounce from 'lodash.debounce';
 import {DropDown, DropDownItem, SearchStyles} from './styles/DropDown';
 
+const SEARCH_ITEMS_QUERY = gql`
+    query SEARCH_ITEMS_QUERY($searchTerm: String!){
+        items(where: {
+            OR: [   { title_contains: $searchTerm },
+                    { description_contains: $searchTerm }
+                ] 
+            }) {
+            id
+            image
+            title
+        }
+    }
+`;
+
 class AutoComplete extends React.Component {
+  onChange = async (e, client) => {
+    // Manually query apollo client
+    const res = await client.query ({
+      query: SEARCH_ITEMS_QUERY,
+      variables: {searchTerm: e.target.value},
+    });
+  };
   render () {
     return (
       <SearchStyles>
         <div>
-          <input type="search" />
+          {/* ApolloConsumer provdes us to manually call the function of apollo client and not only when the page loades  */}
+          <ApolloConsumer>
+            {client => (
+              <input
+                type="search"
+                onChange={e => {
+                  e.persist ();
+                  this.onChange (e, client);
+                }}
+              />
+            )}
+          </ApolloConsumer>
           <DropDown>
             <p>Items will go here</p>
           </DropDown>
